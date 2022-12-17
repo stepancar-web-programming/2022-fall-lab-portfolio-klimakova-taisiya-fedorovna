81228fc4bb8200e2539567c57e23f995a717bf61
TayaKlimakova <96236885+TayaKlimakova@users.noreply.github.com>
2022-12-17 13:43:35 +0300

    Update script.js

--- scrıpt/script.js
+++ scrıpt/script.js
@@ -9,29 +9,29 @@ const imageArray = [
 ];
 
 (function () {
-  var number = 0;
+  let currentImageIndex = 0;
 
   const imageCount = imageArray.length;
-  function flipPictures(num) {
-    if (num == 1) {
-      if (number < imageCount - 1) {
-        number++;
-        document.getElementById('images').src = imageArray[number];
-        document.getElementById('imgNumber').innerHTML = `${number + 1}/${imageCount}`;
+  function flipPictures(isForwardDirection) {
+    if (isForwardDirection) {
+      if (currentImageIndex < imageCount - 1) {
+        currentImageIndex++;
+        document.getElementById('images').src = imageArray[currentImageIndex];
+        document.getElementById('imgNumber').innerHTML = `${currentImageIndex + 1}/${imageCount}`;
       }
-    } else if (number > 0) {
-      number--;
-      document.getElementById('images').src = imageArray[number];
-      document.getElementById('imgNumber').innerHTML = `${number + 1}/${imageCount}`;
+    } else if (currentImageIndex > 0) {
+      currentImageIndex--;
+      document.getElementById('images').src = imageArray[currentImageIndex];
+      document.getElementById('imgNumber').innerHTML = `${currentImageIndex + 1}/${imageCount}`;
     }
   }
   document.write(`<img id="images" src="${imageArray[0]}">`);
 
   function ready() {
-    let leftArrow = document.getElementById('prev');
-    let rightArrow = document.getElementById('next');
-    leftArrow.addEventListener("click", () => flipPictures(0));
-    rightArrow.addEventListener("click", () => flipPictures(1));
+    const previousPictureArrow = document.getElementById('previousPicture');
+    const nextPictureArrow = document.getElementById('nextPicture');
+    previousPictureArrow.addEventListener("click", () => flipPictures(0));
+    nextPictureArrow.addEventListener("click", () => flipPictures(1));
   }
   document.addEventListener("DOMContentLoaded", ready);
 }());
